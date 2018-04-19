678db0dd742299417fce9e320ab03034447408e9
Federico Magnanini <187029@studenti.unimore.it>
2018-04-19 18:41:58 +0200

    First minimal contract structure
    The contract implements a way to purchase the royalty by sending the
    minimum amount requested by the contract creator and also a function
    to check wether an account has already purchased the royalty.
    
    The data structure adopted to keep a record of accounts who have already
    payed accomodates the fact that in Solidity 0.4.21 mappings do not allow
    to check whether a key already exists. In a mapping all possible keys
    exist and all values are initialized to 0.
    In order to do check if a user has already payed a supporting array of
    addresses is needed. The mapping is then used to map an address to an
    index in the aforementioned array.

--- src/contracts/Royalty.sol
+++ src/contracts/Royalty.sol
@@ -2,19 +2,47 @@ pragma solidity ^0.4.21;
 
 contract Royalty {
     address owner;
+    uint32 public resourceHash; //obtained by using sha256
+    string public resourceName; //Expensive
+    //May be used to prove the owner was the first to "register"
+    //the resource in case of a dispute.
+    uint public contractMinedTimestamp;
+    uint256 public minimumPriceWei;
+
+    mapping (address=>uint256) payers;
+    address[] payersIndex;
 
     modifier OnlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    function Royalty() public {
+    function Royalty(uint32 resHash, string resName, uint256 minimumPriceInWei) public {
         owner = msg.sender;
+        resourceHash = resHash;
+        resourceName = resName;        
+        contractMinedTimestamp = block.timestamp;
+        minimumPriceWei = minimumPriceInWei;
     }
 
-    function Buy() public payable {}
+    //Allows the sender to purchase the royalty by sending
+    //at least the minimum amount required. If the sender
+    //has not already payed he is added in a payers collection
+    function Purchase() public payable {
+        require(msg.value >= minimumPriceWei);
+
+        if (!HasAlreadyPayed(msg.sender))
+            payers[msg.sender] = payersIndex.push(msg.sender) - 1;
+    }
 
     function Withdraw() public OnlyOwner {
         owner.transfer(address(this).balance);
+    }
+
+    function HasAlreadyPayed(address account) public view returns (bool) {
+        if (payersIndex.length == 0)
+            return false;
+
+        return (payersIndex[payers[account]] == account);
     }
 }
